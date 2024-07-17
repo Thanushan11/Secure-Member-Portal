4df32651423506d86d4ce46a08dc5da551ca1a7c
Thanushan11 <ravithanu99@gmail.com>
2024-07-17 19:38:20 +0530

    “login_completed”

--- login.php
+++ login.php
@@ -7,27 +7,43 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <?php
-session_start();
-include 'conn.php';
+    session_start();
+    include 'conn.php';
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $email = $_POST['email'];
-    $password = $_POST['password'];
+    if (isset($_POST['submit'])) {
+        $email = $_POST['email'];
+        $password = $_POST['password'];
 
-    $stmt = $pdo->prepare("SELECT * FROM tbl_users WHERE email = ?");
-    $stmt->execute([$email]);
-    $user = $stmt->fetch();
+        $sql = "SELECT * FROM tbl_users WHERE email = :email";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':email', $email);
+        $stmt->execute();
 
-    if ($user && password_verify($password, $user['password'])) {
-        $_SESSION['loggedIn'] = true;
-        $_SESSION['userId'] = $user['userId'];
-        header("Location: protected-home.php");
-        exit;
-    } else {
-        echo "Invalid email or password.";
+        if ($stmt->rowCount() == 1) {
+            $user = $stmt->fetch(PDO::FETCH_ASSOC);
+            if (password_verify($password, $user['password'])) {
+                $_SESSION['user_id'] = $user['id'];
+                echo '<script>
+                        alert("welcome");
+                        
+                      </script>';
+                exit();
+            } else {
+                echo '<script>
+                        alert("Email or password is incorrect. Please try again.");
+                        window.location.href = "login.php";
+                      </script>';
+            }
+        } else {
+            echo '<script>
+                    alert("Email or password is incorrect. Please try again.");
+                    window.location.href = "login.php";
+                  </script>';
+        }
     }
-}
-?>
+    ?>
+    
+
 <h1>Login Page</h1>
 
 <form method="post" action="login.php">
@@ -37,5 +53,5 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <input type="password" id="password" name="password" required><br>
 
     <input type="checkbox" name="remember_me"> Remember Me <br>
-    <button type="submit">Login</button>
+    <input type="submit" id="btn" value="Login" name="submit">
 </form>
